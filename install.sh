124f5753ff79f7c6563e92aa9275cb09ec7f3ec3
naudecle <cleriaaude@gmail.com>
2020-07-05 18:19:37 +0400

    edition de install et updateFancyTools

--- install.sh
+++ install.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
 
+mkdir $HOME/src
 
-way="~/fancy_tools"
+way="$HOME/src/fancy_tools"
 z="source $way/.aliases"
 y="source $way/fancy_function.sh"
 x=$(<~/.bashrc)
+e=$"~/bin"
+
 
 if grep -q "$z" <<< "$x"; then
 			echo "Fichier '.aliases' est deja dans .bashrc"
 else
-	echo source $way/.aliases >> ~/.bashrc
-	echo "Fichier ajoute"
+	git clone https://github.com/naudecle/fancy_tools.git ~/src
+	echo $z >> ~/.bashrc
+	echo "Fichier '.aliases' ajoute"
 fi	
 
 if grep -q "$y" <<< "$x"; then
 			echo "Fichier 'fancy_function.sh' est deja dans .bashrc"
 else
-	echo source $way/fancy_function.sh >> ~/.bashrc
-	echo "Fichier ajoute"
+	echo $y >> ~/.bashrc
+	echo "Fichier 'fancy_function.sh' ajoute"
 fi		
+
+if [ -e "$e" <<< "$HOME" ]
+then
+	
+	cp ~/fancy_tools/bin/updateFancyTools ~/bin/updateFancyTools
+else
+	mkdir ~/bin
+	export PATH=$PATH:$HOME/bin
+	cp ~/fancy_tools/bin/updateFancyTools ~/bin/updateFancyTools
+fi
 
 
 
